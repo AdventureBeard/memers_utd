44b1ecd4a3c77c2c0aea058e887525c400d00b91
Braden Herndon <braden.herndon@gmail.com>
2017-11-07 19:32:03 -0600

    PART 4

--- meme.js
+++ meme.js
@@ -10,48 +10,6 @@ class Meme {
         this.captionCoordinates = captionCoordinates;
         this.captionMaxWidth = captionMaxWidth;
     }
-
-    async create(captions) {
-        if (captions.length != this.captionCoordinates.length) {
-            throw "Wrong number of captions";
-        } else {
-            let captionMap = this._createCaptionMapping(captions);
-            let image = await this._writeCaptionsToImage(captionMap);
-            let imageData = await this._getBuffer(image);
-            return imageData;
-        }
-    }
-
-    _createCaptionMapping(captions) {
-        let captionMap = [];
-        this.captionCoordinates.forEach((coords, i) => {
-            let captionMapping = {
-                x: coords[0],
-                y: coords[1],
-                text: captions[i]
-            }
-            captionMap.push(captionMapping);
-        }) 
-        return captionMap;
-    }
-
-    async _writeCaptionsToImage(captionMap) {
-        let image = await Jimp.read(this.templateImagePath);
-        let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-        captionMap.forEach((e) => {
-            image.print(font, e.x, e.y, e.text, this.captionMaxWidth);
-        });
-        return image;
-    }
-
-    _getBuffer(image) {
-        return new Promise((resolve, reject) => {
-            image.getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
-                if (err) reject();
-                resolve(buffer);
-            })
-        });
-    }
 }
 
 module.exports = Meme;
