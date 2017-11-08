a404503487b4d44aa9af9d851c65a3150ffa522e
Braden Herndon <braden.herndon@gmail.com>
2017-11-07 19:16:06 -0600

    PART 1

--- app.js
+++ app.js
@@ -7,32 +7,8 @@ const fs = require('fs');
 
 app.use(bodyParser.json());
 
-
-const Meme = require('./meme');
 app.route('/memes/expandingbrain').get((req, res) => {
     res.send("Expanding Brain endpoint");
-});
-
-app.route('/memes/expandingbrain').post(async (req, res) => {
-    let body = req.body;
-    let expandingBrainMeme = new Meme(
-        'expand.jpg',
-        [
-            [30, 100],
-            [30, 400],
-            [30, 700],
-            [30, 1000]
-        ],
-        370
-    )
-    try {
-        let imageData = await expandingBrainMeme.create(body.captions);
-        res.set('Content-Type', 'image/jpeg');
-        res.end(imageData);
-    } catch (e) {
-        res.status = 400;
-        res.send(e);
-    }
 });
 
 app.listen(3000, () => console.log("Demo listening on port 3000"));
