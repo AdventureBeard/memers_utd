87426343c068cb37c88ae12eb93aed994684ade1
Braden Herndon <braden.herndon@gmail.com>
2017-11-07 19:51:00 -0600

    hello world

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const path = require('path')
-const fs = require('fs');
 
 app.use(bodyParser.json());
 
 app.route('/memes/expandingbrain').get((req, res) => {
-    res.send("Expanding Brain endpoint");
+    res.send("Hello World!");
 });
 
 app.listen(3000, () => console.log("Demo listening on port 3000"));
