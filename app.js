d6391bc6d405767a5946cd5373e78cc2b3bb99e1
Braden Herndon <braden.herndon@match.com>
2017-11-08 18:05:55 -0600

    Remove extraneous imports

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const path = require('path')
-const fs = require('fs');
 
 app.use(bodyParser.json());
 
